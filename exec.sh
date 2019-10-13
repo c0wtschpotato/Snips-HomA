0d6a1fa794c3c82c49553b2fed82676162c7be87
c0w <c0wtschpotato@gmail.com>
2019-10-13 15:45:16 +0200

    Update exec.sh
    anführungszeichen entfernt damit die befehle laufen.

--- exec.sh
+++ exec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "sudo git pull"
-echo "sudo systemctl restart snips-skill-server"
-echo "sudo chmod +x action-MHZTrigger-HomA.py"
-echo "sudo chmod +x action-hello-HomA.py"
+echo sudo git pull
+echo sudo systemctl restart snips-skill-server
+echo sudo chmod +x action-MHZTrigger-HomA.py
+echo sudo chmod +x action-hello-HomA.py
