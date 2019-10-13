d0c93b4f52789d9873d256f20c20e1e3dc4b5cca
c0w <c0wtschpotato@gmail.com>
2019-10-13 15:10:18 +0200

    Update action-MHZTrigger-HomA.py
    funktionierende Version mit grundlegendener if Verschachtelung

--- action-MHZTrigger-HomA.py
+++ action-MHZTrigger-HomA.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
@@ -6,17 +7,25 @@ import socket, time
 
 def action_wrapper(hermes, intent_message):
 
-    first = int(intent_message.slots.Geraet.first().value)
-    second = int(intent_message.slots.AnAus.first().value)
+    first = intent_message.slots.Geraet.first().value
+    second = intent_message.slots.AnAus.first().value
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(('192.168.1.107', 10000))
-    s.send(b'11001-'+int(first)+'-'+int(second))
+    if first == "couch" or first == "sofa"  and second == 1:
+
+        s.send(b'11001-3-1')
+        result_sentence = "An wurde gesendet"
+    else:
+        result_sentence = ""
+    time.sleep(3)
+    s.send(b'11001-3-0')
     s.close()
-    result_sentence = "Okay"+first+" "+second
+    result_sentence = result_sentence + " " + first
     current_session_id = intent_message.session_id
     hermes.publish_end_session(current_session_id, result_sentence)
 
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("domi:getAddition", action_wrapper).start()
+        h.subscribe_intent("c0wtschpotato:MHZTrigger", action_wrapper).start()
+
