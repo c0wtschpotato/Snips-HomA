4feb5c6cda878e0953a8bc4422e161a2d7c149d4
c0w <c0wtschpotato@gmail.com>
2019-10-14 19:10:12 +0200

    Update action-CouchFarbe-HomA.py
    return statt exit

--- action-CouchFarbe-HomA.py
+++ action-CouchFarbe-HomA.py
@@ -16,8 +16,7 @@ def action_wrapper(hermes, intent_message):
 		current_session_id = intent_message.session_id
 		hermes.publish_end_session(current_session_id, result_sentence)
 		s.close()
-		sys.exit()
-		exit()
+		return
 	except:
 		result_sentence = "Wie bitte?"
 		current_session_id = intent_message.session_id
