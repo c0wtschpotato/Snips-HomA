b6e1e7790a5f05abab37f1eb421e6bcbd4321fee
c0w <c0wtschpotato@gmail.com>
2019-10-18 15:29:44 +0200

    Update action-Conversatinal-HomA.py

--- action-Conversatinal-HomA.py
+++ action-Conversatinal-HomA.py
@@ -7,7 +7,7 @@ def action_wrapper(hermes, intent_message):
 
 
 
-
+	hermes.publish_end_session(current_session_id, "no code here")
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
         h.subscribe_intent("c0wtschpotato:CouchFarbe", action_wrapper).start()
