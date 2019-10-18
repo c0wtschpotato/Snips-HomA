9dfff293a3af117f658484ee31031c406fe4f8d6
c0w <c0wtschpotato@gmail.com>
2019-10-18 14:07:15 +0200

    Update action-IR-Homa.py

--- action-IR-Homa.py
+++ action-IR-Homa.py
@@ -1,13 +1,16 @@
 from hermes_python.hermes import Hermes
 import socket, time, sys
 
+intent_choose_device = "player"
+intent_command = "Befehl"
+
 
 def action_wrapper(hermes, intent_message):
 
-
+	hermes.publish_continue_session(intent_message.session_id, sentence, [        INTENT_ANSWER,        INTENT_INTERRUPT,        INTENT_DOES_NOT_KNOW    ])
 
 
 
 if __name__ == "__main__":
-    with Hermes("localhost:1883") as h:
-        h.subscribe_intent("c0wtschpotato:CouchFarbe", action_wrapper).start()
+    with Hermes(MQTT_ADDR) as h:
+        h.subscribe_intent("c0wtschpotato:ControlPart", conversational_control).start()
