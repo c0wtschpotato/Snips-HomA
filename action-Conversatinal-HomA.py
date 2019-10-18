feb280c0d258844b60ddb15b7b99204744b57dcf
c0w <c0wtschpotato@gmail.com>
2019-10-18 15:31:56 +0200

    Update action-Conversatinal-HomA.py

--- action-Conversatinal-HomA.py
+++ action-Conversatinal-HomA.py
@@ -4,10 +4,13 @@ import socket, time, sys
 
 def action_wrapper(hermes, intent_message):
 
+	if len(intent_message) <= 0:
+		sentence = "Welches Gerät?"
+	else:
+		sentence = "kein code"
 
 
-
-	hermes.publish_end_session(current_session_id, "no code here")
+	hermes.publish_end_session(current_session_id, sentence)
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
         h.subscribe_intent("c0wtschpotato:CouchFarbe", action_wrapper).start()
