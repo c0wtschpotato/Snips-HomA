c17417aa0ec59861e0b250118aefb2c1c2175aa1
c0w <c0wtschpotato@gmail.com>
2019-10-19 15:21:24 +0200

    Update action-Conversatinal-HomA.py

--- action-Conversatinal-HomA.py
+++ action-Conversatinal-HomA.py
@@ -3,7 +3,8 @@
 
 from hermes_python.hermes import Hermes
 import socket, time, sys
-
+intent_hts = "player"
+intent_fertig = "ende"
 
 def action_wrapper(hermes, intent_message):
 	current_session_id = intent_message.session_id
@@ -11,11 +12,13 @@ def action_wrapper(hermes, intent_message):
 
 	if len(str(intent_message)) <= 0:## switched to string for len
 		sentence = "Welches Gerät?"
+		print("länge war 0")
 	else:
-		sentence = "kein code"
+		sentence = "okay, weiter"
+		print("message erkannt")
+		hermes.publish_continue_session(intent_message.session_id,sentence,)
 
-
-	hermes.publish_end_session(current_session_id, sentence)
+	hermes.publish_end_session(intent_message.session_id, sentence,[intent_hts,intent_fertig])
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
         h.subscribe_intent("c0wtschpotato:Conversational", action_wrapper).start()
