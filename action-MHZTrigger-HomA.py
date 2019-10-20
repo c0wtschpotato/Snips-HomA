fd2ecae6c62895ca994635e90d62b14e1e4cb5d7
c0w <c0wtschpotato@gmail.com>
2019-10-20 19:38:50 +0200

    Update action-MHZTrigger-HomA.py

--- action-MHZTrigger-HomA.py
+++ action-MHZTrigger-HomA.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from hermes_python.hermes import Hermes
-import socket, time, ConfigParser
+import socket, time, configparser
 syn_couch = ["couch","sofa","led", "LED","kautsch"]## eigentlich sinnlos, von snips wird nur die eingestellte intent übertragen. synonyme müssen in der console gepflegt werden.
 syn_iiyama = ["bildschirm","screen", "ijama","iiyama","iyama","iljama","kleiner","monitor"]
 syn_fernseher =["fernseher", "großer", "groß", "gross", "philips","filips"]
