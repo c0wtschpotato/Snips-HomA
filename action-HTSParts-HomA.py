b53e00e49cc444cbb76c5f32c4087ec1f6ad4d4e
c0w <c0wtschpotato@gmail.com>
2019-10-26 11:36:38 +0200

    Update action-HTSParts-HomA.py

--- action-HTSParts-HomA.py
+++ action-HTSParts-HomA.py
@@ -40,7 +40,7 @@ def action_wrapper(hermes, intent_message):
 	if first == "bluetooth":
 		config["philips"]["targetchannel"] = "2"
 		result_sentence = ""
-	if first == "pc an"
+	if first == "pc an":
 		config["11001"]["1"] = "1"
 		time.sleep(0.2)
 		config["11001"]["2"] = "1"
