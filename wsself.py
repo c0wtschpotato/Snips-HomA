4aa6fc85cb783e0c96e65881ec16f10e1fd5b86a
c0w <c0wtschpotato@gmail.com>
2019-10-26 02:11:55 +0200

    Update wsself.py

--- wsself.py
+++ wsself.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from ws2801example import rainbow_cycle, rainbow_cycle_successive
+from ws2801example import rainbow_cycle
 import time
 import RPi.GPIO as GPIO
 from threading import Thread 
@@ -18,10 +18,25 @@ pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_
  
 
 
+def rainbow_cycle_successive(pixels, wait=0.1, keeprunning = True):
+	while getattr(t,"keeprunning",True):
+	    for i in range(pixels.count()):
+	        # tricky math! we use each pixel as a fraction of the full 96-color wheel
+	        # (thats the i / strip.numPixels() part)
+	        # Then add in j which makes the colors go around per pixel
+	        # the % 96 is to make the wheel cycle around
+	        pixels.set_pixel(i, wheel(((i * 256 // pixels.count())) % 256) )
+	        pixels.show()
+	        if wait > 0:
+	            time.sleep(wait)
+	print("stopped by getattr")
+
 #while 1:
 
-Thread(target=rainbow_cycle_successive,args=(pixels,0.2 )).start()
-print ("finished cycle")
+t = Thread(target=rainbow_cycle_successive,args=(pixels,0.2,True )).start()
+time.sleep(2)
+print("waited 2")
+t.keeprunning = False
 pixels.show()
 pixels.clear()
 pixels.show() 
