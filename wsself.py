c0a6bcb21fe93bc3d1ca3dd4297601ea43c1a4d4
c0w <c0wtschpotato@gmail.com>
2019-10-27 12:26:59 +0100

    Update wsself.py

--- wsself.py
+++ wsself.py
@@ -21,25 +21,25 @@ pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_
 def rainbow_cycle_successive(pixels, wait=0.1):
 
 	t = threading.currentThread()
-	while getattr(t,"do_run",True):
-	    for i in range(pixels.count()):
-	        # tricky math! we use each pixel as a fraction of the full 96-color wheel
-	        # (thats the i / strip.numPixels() part)
-	        # Then add in j which makes the colors go around per pixel
-	        # the % 96 is to make the wheel cycle around
-	        pixels.set_pixel(i, wheel(((i * 256 // pixels.count())) % 256) )
-	        pixels.show()
-	        pixels.clear()
-	        print(str(i))
-	        if getattr(t,"do_run", False):
-	        	pixels.clear()
-	        	print("stopped by if getattr")
-	        	break
-	        else:
-	        	print ("do_run is on")
-	        	print(getattr(t,"do_run"))
-	        if wait > 0:
-	            time.sleep(wait)
+	
+	for i in range(pixels.count()):
+	    # tricky math! we use each pixel as a fraction of the full 96-color wheel
+	    # (thats the i / strip.numPixels() part)
+	    # Then add in j which makes the colors go around per pixel
+	    # the % 96 is to make the wheel cycle around
+	    pixels.set_pixel(i, wheel(((i * 256 // pixels.count())) % 256) )
+	    pixels.show()
+	    pixels.clear()
+	    print(str(i))
+	    if getattr(t,"do_run", False):
+	    	pixels.clear()
+	    	print("stopped by if getattr")
+	    	break
+	    else:
+	    	print ("do_run is on")
+	    	print(getattr(t,"do_run"))
+	    if wait > 0:
+	        time.sleep(wait)
 	pixels.clear()
 	print("stopped by getattr")
 	print(getattr(t,"do_run"))
