17446432922d96f90879b1c2a36a50969654aaa1
c0w <c0wtschpotato@gmail.com>
2019-10-27 13:14:19 +0100

    Update ws2801effects.py

--- ws2801effects.py
+++ ws2801effects.py
@@ -14,7 +14,8 @@ PIXEL_COUNT = 50
 SPI_PORT   = 0
 SPI_DEVICE = 0
 pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE), gpio=GPIO)
- 
+#defekte Pixel 0,1,43
+defekt = [0,1,43]
  
 # Define the wheel function to interpolate between different hues.
 def wheel(pos):
@@ -93,6 +94,15 @@ def appear_from_back(pixels, color=(255, 0, 0)):
             pixels.set_pixel(j, Adafruit_WS2801.RGB_to_color( color[0], color[1], color[2] ))
             pixels.show()
             time.sleep(0.001)
+
+def running_on_chain(pixels,basecolor = (255,255,255),runningcolor= (255,0,0),number_of_running=(5)):
+    for i in range(pixels.count()):
+        pixels.set_pixel(i,Adafruit_WS2801.RGB_to_color(basecolor[0],basecolor[1],basecolor[2]))
+    pixels.show()
+    for i in range(0,number_of_running):
+        pixels.set_pixel(i,Adafruit_WS2801.RGB_to_color(runningcolor[0],runningcolor[1],runningcolor[2]))
+    pixels.show()
+
             
  
 if __name__ == "__main__":
