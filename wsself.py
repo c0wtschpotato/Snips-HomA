8c01f1813bdaa93b41bfdd1b0439763e579f0a33
c0w <c0wtschpotato@gmail.com>
2019-10-27 12:31:58 +0100

    Update wsself.py

--- wsself.py
+++ wsself.py
@@ -22,18 +22,18 @@ def rainbow_cycle_successive(pixels, wait=0.1):
 
 	for i in range(pixels.count()):
 		global do_run
-	    # tricky math! we use each pixel as a fraction of the full 96-color wheel
-	    # (thats the i / strip.numPixels() part)
-	    # Then add in j which makes the colors go around per pixel
-	    # the % 96 is to make the wheel cycle around
-	    pixels.set_pixel(i, wheel(((i * 256 // pixels.count())) % 256) )
-	    pixels.show()
-	    pixels.clear()
-	    print(str(i))
-	    if do_run:
-	    	break
+		# tricky math! we use each pixel as a fraction of the full 96-color wheel
+		# (thats the i / strip.numPixels() part)
+		# Then add in j which makes the colors go around per pixel
+		# the % 96 is to make the wheel cycle around
+		pixels.set_pixel(i, wheel(((i * 256 // pixels.count())) % 256) )
+		pixels.show()
+		pixels.clear()
+		print(str(i))
+		if do_run:
+			break
 		if wait > 0:
-	        time.sleep(wait)
+	  		    time.sleep(wait)
 	pixels.clear()
 	print("stopped by getattr")
 	print(getattr(t,"do_run"))
