112f0f2efd7f9382a7869fbdccef1e02d6877e35
c0w <c0wtschpotato@gmail.com>
2019-10-27 14:07:00 +0100

    Update ws2801effects.py

--- ws2801effects.py
+++ ws2801effects.py
@@ -104,7 +104,8 @@ def running_on_chain(pixels,basecolor = (255,255,255),runningcolor= (255,0,0),nu
 
     pixels.show()
     for i in range(pixels.count()):
-        
+        if i+number_of_running >= pixels.count():
+            break
         for k in range(pixels.count()):
             pixels.set_pixel(k,Adafruit_WS2801.RGB_to_color(basecolor[0],basecolor[1],basecolor[2]))
         
@@ -113,7 +114,8 @@ def running_on_chain(pixels,basecolor = (255,255,255),runningcolor= (255,0,0),nu
             pixels.set_pixel(j,Adafruit_WS2801.RGB_to_color(runningcolor[0],runningcolor[1],runningcolor[2]))
             
         pixels.show()         
-        time.sleep(sleep_time)        
+        time.sleep(sleep_time)
+        
 
             
  
