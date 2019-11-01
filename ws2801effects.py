5bf8f8cacdfe0107c6e4152e7b239efd09795926
c0w <c0wtschpotato@gmail.com>
2019-11-01 19:25:47 +0100

    Update ws2801effects.py

--- ws2801effects.py
+++ ws2801effects.py
@@ -147,10 +147,7 @@ def setalltocolor(pixels,color=(255,255,255)):
     for i in range(pixels.count()):
         pixels.set_pixel(i, Adafruit_WS2801.RGB_to_color(color[0],color[1],color[2]))#
     pixels.show()
-        
 
-            
- 
 if __name__ == "__main__":
     # Clear all the pixels to turn them off.
     pixels.clear()
