4ae21d7f6b185c60115b12f66adadd7e3c326377
c0w <c0wtschpotato@gmail.com>
2019-11-01 19:00:26 +0100

    Update action-CouchFarbe-HomA.py

--- action-CouchFarbe-HomA.py
+++ action-CouchFarbe-HomA.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
 ###make it executable, maybe git desktop overwrites
 from hermes_python.hermes import Hermes
 import socket, time, sys, configparser, os,json
