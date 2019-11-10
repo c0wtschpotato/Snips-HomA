aa98984f17701bbbadbba9c78090f517fe31c889
c0w <c0wtschpotato@gmail.com>
2019-11-10 15:24:13 +0100

    Update action-PCcontrol-HomA.py

--- action-PCcontrol-HomA.py
+++ action-PCcontrol-HomA.py
@@ -12,7 +12,6 @@ def action_wrapper(hermes, intent_message):
 		channel = intent_message.slots.channel.first().value
 	except:
 		channel = "media"
-	print("publishing message to"+str(intent_message.slots.message.first().value))
 	client.publish("HomA/"+channel,intent_message.slots.message.first().value)
 	print("published message"+ channel" "+intent_message.slots.message.first().value)
 	current_session_id = intent_message.session_id
